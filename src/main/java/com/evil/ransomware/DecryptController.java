1335c70a8072ed7a94df9f14b4c924d551363c1e
Asankhaya Sharma <codelion@users.noreply.github.com>
2017-05-17 08:05:37 +0800

    Update DecryptController.java

--- src/main/java/com/evil/ransomware/DecryptController.java
+++ src/main/java/com/evil/ransomware/DecryptController.java
@@ -2,8 +2,5 @@ package com.evil.ransomware;
 
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
-/**
- * Created by caleb on 3/22/16.
- */
 public class DecryptController extends SimpleUrlHandlerMapping {
 }
